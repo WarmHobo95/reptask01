38800a910e819a57953e6d8be16c57c595521728
pc <a@mail>
2023-09-30 11:39:14 +0500

    task_done2

--- bin/project.dart
+++ bin/project.dart
@@ -1,4 +1,3 @@
-import 'package:project/project.dart' as project;
 import 'package:dio/dio.dart';
 
 void main(List<String> arguments) async {
@@ -6,15 +5,11 @@ void main(List<String> arguments) async {
   var url = await client.get('https://dummyjson.com/products');
   var data1 = url.data;
   double a = 0;
-  //print(url.data);
-  Map<String,dynamic> dataprod = data1;
+  Map<String, dynamic> dataprod = data1;
   List<dynamic> prods = dataprod['products'];
-    for(dynamic p in prods){
-      //print(p['rating']);
-      a += p['rating'];
-    }
+  for (dynamic p in prods) {
+    a += p['rating'];
+  }
   a = a / prods.length;
   print(a);
-
-
 }

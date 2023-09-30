13d66c6a91365c8dde29f6594c17de1cc00aaca7
pc <a@mail>
2023-09-30 10:42:29 +0500

    task1_done

--- bin/project.dart
+++ bin/project.dart
@@ -1,5 +1,20 @@
 import 'package:project/project.dart' as project;
+import 'package:dio/dio.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${project.calculate()}!');
+void main(List<String> arguments) async {
+  var client = Dio();
+  var url = await client.get('https://dummyjson.com/products');
+  var data1 = url.data;
+  double a = 0;
+  //print(url.data);
+  Map<String,dynamic> dataprod = data1;
+  List<dynamic> prods = dataprod['products'];
+    for(dynamic p in prods){
+      //print(p['rating']);
+      a += p['rating'];
+    }
+  a = a / prods.length;
+  print(a);
+
+
 }
